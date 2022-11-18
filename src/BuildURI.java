69e9493a89d72d9f31740dfafd0b039d0ce863ca
Sean White <seanbrwh@gmail.com>
2022-11-18 15:39:44 -0700

    working on env

--- src/BuildURI.java
+++ src/BuildURI.java
@@ -1,5 +1,9 @@
 import java.sql.Timestamp;
+import io.github.cdimascio.dotenv.Dotenv;
 public class BuildURI {
+    Dotenv dotenv = Dotenv.load();
     private final String BASE_URL = "https://gateway.marvel.com:443/v1/public/";
+    private final String PUBLIC_KEY = dotenv.get("PUBLIC_KEY");
+    private final String PRIVATE_KEY = dotenv.get("PRIVATE_KEY");
 
 }
